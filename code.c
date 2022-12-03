83369ca9b62aa3db66a6b918becffc91b358fb5e
oAmirrezao <119684561+oAmirrezao@users.noreply.github.com>
2022-12-03 11:41:17 +0330

    update4 code.c

--- code.c
+++ code.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-  int a;
-  printf("Please enter a number:");
-  scanf("%d",&a);
-  printf("yoru number is : %d",a);
+  int a,b;
+  printf("Please enter two numbers:");
+  scanf("%d %d",&a,&b);
+  printf("%d+%d = %d",a,b,a+b);
   return 0;
 }
